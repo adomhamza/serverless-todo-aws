8bedd7fbc7273aac2d730ef27ece10dfb0871f58
Adom Hamza <adomhamza@Adoms-MacBook-Air.local>
2020-08-28 15:19:49 +0000

    Added to config.ts

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,5 +1,5 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = 'u8hc4r94p1'
+const apiId = '8mp2pzo6l2'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
